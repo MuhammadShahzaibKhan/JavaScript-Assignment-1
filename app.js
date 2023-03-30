31fa1a32e306f48e6d69dabbd49b4927866a7f45
MuhammadShahzaibKhan <mshahzaibkhan17@gmail.com>
2023-03-30 22:07:54 +0500

    Assignment Complete

---
+++ app.js
@@ -0,0 +1,91 @@
+// // Chapter 1 => Alerts
+
+// alert("Welcome to JavaScript");
+
+// // Chapter 2 => Variable for Strings
+
+// var name = "Muhammad Shahzaib Khan";
+// console.log(name);
+
+// // Chapter 3 => Variable for Numbers
+
+// var cost = 150;
+// console.log(cost);
+
+// // Chapter 6 => Variable legal and Illegal
+
+// var name = "Shahzaib" // Legal
+// var name1 = "Shahzaib" // Legal
+// var $name = "Shahzaib" // Legal
+// var firstName = "Shahzaib" // legal
+// var first_name = "Shahzaib" // Legal
+
+// var 123 = "Shahzaib" // Illegal
+// var %&#- = "Shahzaib" // all special character are illegal except _ and $
+// var 2name = "Shahzaib" // Illegal
+
+// // Chapter 5 => Maths Expressions: Family Operators
+
+// var a = 50;
+// var b = 40;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// // Chapter 6 => Mathe Expressions Unfamiliar Operators
+
+// var a = 10;
+// console.log(a++);
+// console.log(++a);
+// console.log(--a);
+// console.log(a--);
+
+// // Chapter 7 => Maths Expressions Eliminating Ambiguity
+
+// var foodCost = 10;
+// var  RideCost = 20;
+// var tip = 40;
+// var total = (10 + 20) / 5;
+// console.log(total);
+
+// // Chapter 8 => Concatenating Text Strings
+
+// var firstName = "Muhammad Shahzaib ";
+// var lastName = "Khan";
+// var city = "Karachi";
+// console.log(firstName + lastName + " From " + city);
+
+// // Chapter 9 => Prompts
+
+// var firstName = prompt("Enter Your first name ");
+// var lastName = prompt("Enter Your last name");
+// console.log(firstName + " " + lastName);
+
+// // Chapter 10 => if Statements
+
+// var chandNazarAgya = true;
+
+// if(chandNazarAgya == true){
+//     console.log("EID MUBARAK🎆🎇");
+// }else{
+//     console.log("Kl bhi roza hai");
+// }
+
+// // Chapter 11 => Comparison operators
+
+// var a = "10";
+// var b = 10;
+
+// if(a == b){
+//     console.log(a + b);
+// }else{
+//     console.log("Invalid");
+// }
+
+// if(a === b){
+//     console.log(a + b);
+// }else{
+//     console.log("Invalid");
+// }
